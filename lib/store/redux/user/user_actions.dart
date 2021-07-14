7363fde69db39772d8fc8c8902545973f5679cec
Madek1 <michubilu@wp.pl>
2021-07-14 20:11:24 +0200

    Added log in action

--- lib/store/redux/user/user_actions.dart
+++ lib/store/redux/user/user_actions.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:redux/redux.dart';
+import 'package:untitled/store/models/user_model.dart';
 import 'package:untitled/store/redux/user/user_state.dart';
+import 'package:untitled/store/store.dart';
 
 @immutable
 abstract class UserStateActions {
@@ -15,7 +18,17 @@ class SetUserStateAction extends UserStateActions {
   SetUserStateAction(UserState userState) : super(userState);
 }
 
-@immutable
-class ClearUserStateAction extends UserStateActions {
-  ClearUserStateAction(UserState userState) : super(userState);
+Future<void> logIn(Store<AppState> store) async {
+  store.dispatch(
+    SetUserStateAction(
+      UserState(
+        isLogging: false,
+        user: UserModel(
+          uid: 'c7282cf2-e4ce-11eb-ba80-0242ac130004',
+          name: 'Michal',
+          surname: 'Bilski',
+        ),
+      ),
+    ),
+  );
 }
