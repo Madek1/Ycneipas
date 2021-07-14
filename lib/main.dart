48207414da61f4a981bed71514ac1f45caf927c0
Madek1 <michubilu@wp.pl>
2021-07-14 22:07:49 +0200

    Init firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:untitled/app.dart';
 import 'package:untitled/store/store.dart';
 
 Future<void> main() async {
   await Redux.init();
+  await Firebase.initializeApp();
+  await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
   runApp(App());
 }
