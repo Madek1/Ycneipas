44dce2b4e106e98d39b4123107bdd14e9a5bd869
Madek1 <michubilu@wp.pl>
2021-07-14 19:30:32 +0200

    Adde wallets state to global state

--- lib/store/store.dart
+++ lib/store/store.dart
@@ -3,20 +3,27 @@ import 'package:redux/redux.dart';
 import 'package:untitled/store/redux/user/user_actions.dart';
 import 'package:untitled/store/redux/user/user_reducer.dart';
 import 'package:untitled/store/redux/user/user_state.dart';
+import 'package:untitled/store/redux/wallet/wallet_actions.dart';
+import 'package:untitled/store/redux/wallet/wallet_reducer.dart';
+import 'package:untitled/store/redux/wallet/wallet_state.dart';
 
 @immutable
 class AppState {
   final UserState userState;
+  final WalletsState walletsState;
 
   AppState({
     required this.userState,
+    required this.walletsState,
   });
 
   AppState copyWith({
     UserState? userState,
+    WalletsState? walletsState,
   }) {
     return AppState(
       userState: userState ?? this.userState,
+      walletsState: walletsState ?? this.walletsState,
     );
   }
 }
@@ -26,6 +33,15 @@ AppState appReducer(AppState state, dynamic action) {
     final UserState newUserState = userReducer(state.userState, action);
     return state.copyWith(
       userState: newUserState,
+    );
+  }
+  if (action is WalletStateActions) {
+    final WalletsState newWalletsState = walletsReducer(
+      state.walletsState,
+      action,
+    );
+    return state.copyWith(
+      walletsState: newWalletsState,
     );
   }
   return state;
@@ -43,11 +59,13 @@ class Redux {
 
   static Future<void> init() async {
     final UserState initialUserState = UserState.initial();
+    final WalletsState initialWalletsState = WalletsState.initial();
     _store = Store<AppState>(
       appReducer,
       middleware: [],
       initialState: AppState(
         userState: initialUserState,
+        walletsState: initialWalletsState,
       ),
     );
   }
