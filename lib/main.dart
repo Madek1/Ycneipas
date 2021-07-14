7878c5c82d6cece77a4dc7990fb5b4001d0035e1
Madek1 <michubilu@wp.pl>
2021-07-14 22:02:30 +0200

    Added firebase auth package

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:untitled/app.dart';
 import 'package:untitled/store/store.dart';
 
-void main() async {
+Future<void> main() async {
   await Redux.init();
   runApp(App());
 }
