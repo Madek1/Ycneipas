3db4b721710f4e72b1936b728ad0de8afad1f749
Madek1 <michubilu@wp.pl>
2021-07-15 20:09:05 +0200

    Fixed firebase init

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:untitled/app.dart';
@@ -6,7 +5,14 @@ import 'package:untitled/store/store.dart';
 
 Future<void> main() async {
   await Redux.init();
-  await Firebase.initializeApp();
-  await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase?.initializeApp(
+    options: FirebaseOptions(
+      apiKey: 'AIzaSyBlTrF_6gP8IdFx-uG4aFtWGNsMX2Mr1gM',
+      appId: '1:113579661601:web:6305378cba623e89068660',
+      messagingSenderId: '113579661601',
+      projectId: 'tipbro',
+    ),
+  );
   runApp(App());
 }
