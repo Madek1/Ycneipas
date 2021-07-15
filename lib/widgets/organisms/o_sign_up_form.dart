fd896211d5094ddcff0f7c7af309e475cce891f1
Madek1 <michubilu@wp.pl>
2021-07-15 20:57:14 +0200

    Added registration page

---
+++ lib/widgets/organisms/o_sign_up_form.dart
@@ -0,0 +1,106 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
+import 'package:untitled/store/models/user_model.dart';
+import 'package:untitled/store/redux/user/user_actions.dart';
+import 'package:untitled/store/redux/user/user_state.dart';
+import 'package:untitled/store/store.dart';
+
+final FirebaseAuth _auth = FirebaseAuth.instance;
+
+class OSignInForm extends StatefulWidget {
+  const OSignInForm();
+
+  @override
+  _OSignInFormState createState() => _OSignInFormState();
+}
+
+class _OSignInFormState extends State<OSignInForm> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _email = TextEditingController();
+  final TextEditingController _password = TextEditingController();
+
+  Future<void> _register() async {
+    try {
+      final User user = (await _auth.createUserWithEmailAndPassword(
+        email: _email.text,
+        password: _password.text,
+      ))
+          .user!;
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Signed up'),
+        ),
+      );
+      Redux.store.dispatch(
+        SetUserStateAction(
+          UserState(
+            user: UserModel(
+              firebaseDetails: user,
+            ),
+          ),
+        ),
+      );
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Failed to sign up'),
+        ),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              TextFormField(
+                controller: _email,
+                keyboardType: TextInputType.emailAddress,
+                decoration: const InputDecoration(labelText: 'Email'),
+                validator: (String? value) {
+                  if (value!.isEmpty) {
+                    return 'Required';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: _password,
+                decoration: const InputDecoration(labelText: 'Password'),
+                validator: (String? value) {
+                  if (value!.isEmpty) {
+                    return 'Required';
+                  }
+                  return null;
+                },
+                obscureText: true,
+                autocorrect: false,
+                enableSuggestions: false,
+              ),
+              Container(
+                padding: const EdgeInsets.only(top: 16),
+                alignment: Alignment.center,
+                child: SignInButton(
+                  Buttons.Email,
+                  text: 'Sign In',
+                  onPressed: () async {
+                    if (_formKey.currentState!.validate()) {
+                      await _register();
+                    }
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/pages/auth/register.dart
@@ -0,0 +1,37 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:untitled/utils/todo.dart';
+import 'package:untitled/widgets/organisms/o_sign_in_form.dart';
+import 'package:untitled/widgets/templates/t_page.dart';
+
+final FirebaseAuth _auth = FirebaseAuth.instance;
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage();
+
+  @override
+  _RegisterPageState createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  User? user;
+
+  @override
+  void initState() {
+    super.initState();
+    _auth.userChanges().listen((event) => setState(() => user = event));
+  }
+
+  @override
+  @TODO('Create form with email and password inputs, sign in button')
+  Widget build(BuildContext context) {
+    return TPage(
+      appBar: AppBar(
+        title: Text('Sign in'),
+      ),
+      body: <Widget>[
+        OSignInForm(),
+      ],
+    );
+  }
+}
